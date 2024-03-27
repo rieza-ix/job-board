eb7d22cfe97f263826ec4f00e5d964fbb70419dd
rieza-ix <rizmarezlo@gmail.com>
2024-03-27 16:26:35 +0800

    :lipstick: feat: design home page

--- components/NavBar.vue
+++ components/NavBar.vue
@@ -3,7 +3,8 @@
         <nav class="bg-[#ebf4f5] border-gray-200 dark:bg-gray-900 shadow-md">
             <div class="flex justify-between items-center mx-auto max-w-screen-xl py-4 md:px-8 px-4">
                 <NuxtLink to="#" class="flex items-center md:space-x-3 space-x-2 rtl:space-x-reverse">
-                    <span class="self-center md:text-2xl text-lg font-bold whitespace-nowrap dark:text-white">Job Posting
+                    <span class="self-center md:text-2xl text-lg font-bold whitespace-nowrap dark:text-white">Job
+                        Posting
                         Board</span>
                 </NuxtLink>
                 <div class="flex items-center md:space-x-3 space-x-2 rtl:space-x-reverse">
